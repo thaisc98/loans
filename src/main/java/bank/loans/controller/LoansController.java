168434ee38a1d09b504e3aea8b06eb285a314f0f
thaisc98 <thaisc98@hotmail.es>
2022-12-14 21:06:53 -0400

    [FIX] github

--- src/main/java/bank/loans/controller/LoansController.java
+++ src/main/java/bank/loans/controller/LoansController.java
@@ -1,9 +1,15 @@
 package bank.loans.controller;
 
+import bank.loans.config.LoansServiceConfig;
 import bank.loans.model.Customer;
 import bank.loans.model.Loans;
+import bank.loans.model.Properties;
 import bank.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +23,21 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    LoansServiceConfig loansServiceConfig;
+
     @PostMapping("/my-loans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer){
         List<Loans> loans = loansRepository.findByCustomerIdOrderByStartAtDesc(customer.getCustomerId());
         return Objects.nonNull(loans) ? loans : null;
     }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow =  new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansServiceConfig.getMsg(), loansServiceConfig.getBuildVersion(),
+                loansServiceConfig.getMailDetails(), loansServiceConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
+    }
+
 }
